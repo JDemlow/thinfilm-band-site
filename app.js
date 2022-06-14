4815cb53f05d7288d565303149751d8aba30a1a4
James Demlow <jdemlow@gmail.com>
2022-06-13 23:25:21 -0600

    thinfilm band website
    website for local band

---
+++ app.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("menu__btn");
+const nav = document.getElementById("menu");
+
+function navToggle() {
+  btn.classList.toggle("open");
+  nav.classList.toggle("hidden");
+  document.body.classList.toggle("no__scroll");
+}
+
+btn.addEventListener("click", navToggle);
